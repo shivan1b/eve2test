7ad2e69122b65e62821a4af46f5a23f42dc889bb
Shivani Bhardwaj <shivanib134@gmail.com>
2019-05-06 09:51:04 +0530

    Add parsers

--- eve2test/main.py
+++ eve2test/main.py
@@ -1,5 +1,18 @@
+import json
+
+from eve2test import parser
+
+
+def process_eve(path):
+    with open(path, "r") as fp:
+        content = json.load(fp)
+    print(content.keys())
+
+
 def main():
-    print("Success")
+    eve_path, output_path = parser.parse_args()
+    process_eve(path=eve_path)
+    print("Success", eve_path, output_path)
 
 
 if __name__ == "__main__":
---
+++ eve2test/parser.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Convert eve.json to test.yaml")
+    parser.add_argument("path-to-eve", metavar="<path-to-eve>",
+            help="Path to eve.json")
+    parser.add_argument("output-path", metavar="<output-path>",
+            help="Path to the folder where generated test.yaml should be put")
+
+    args = parser.parse_args()
+
+    eve_path = getattr(args, "path-to-eve")
+    output_path = getattr(args, "output-path")
+    return eve_path, output_path
