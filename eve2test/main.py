49b3fab00abd61be12514319ed5a86904bc6bc01
Shivani Bhardwaj <shivanib134@gmail.com>
2019-07-08 21:10:43 +0530

    Add license, sort headers

--- eve2test/main.py
+++ eve2test/main.py
@@ -1,13 +1,29 @@
+# Copyright (C) 2019 Open Information Security Foundation
+#
+# You can copy, redistribute or modify this Program under the terms of
+# the GNU General Public License version 2 as published by the Free
+# Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# version 2 along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
 import json
-import yaml
 import logging
 import os
 import sys
 from collections import defaultdict
+
+import yaml
 from yaml.representer import Representer
 
 from eve2test import parser
-
 
 # Get a logger instance
 logger = logging.getLogger(__name__)
